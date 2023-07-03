41cd11ae2ebb1a0dfaebdc37334f9f6cf16418f6
harper.wen <owilalala974@gmail.com>
2023-07-03 17:16:34 +0800

    refactor: add before hook

--- spec/burger_spec.rb
+++ spec/burger_spec.rb
@@ -1,12 +1,18 @@
 require './burger'
 
 RSpec.describe Burger do
+
+  before do
+    p "Hello, this is before hooks"
+    @burger = Burger.new('Beef', 'Cheddar')
+  end
+
   it 'has cheese' do
-    burger = Burger.new('Beef', 'Cheddar')
-    expect(burger.cheese).to eq('Cheddar')
+    # burger = Burger.new('Beef', 'Cheddar')
+    expect(@burger.cheese).to eq('Cheddar')
   end
   it 'has meat' do
-    burger = Burger.new('Beef', 'Cheddar')
-    expect(burger.meat).to eq('Beef')
+    # burger = Burger.new('Beef', 'Cheddar')
+    expect(@burger.meat).to eq('Beef')
   end
 end
