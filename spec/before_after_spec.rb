21ee08ab88e14f57fc795c0fedd696bb5a9d06d6
harper.wen <owilalala974@gmail.com>
2023-07-04 11:19:43 +0800

    feat: add context method

--- spec/before_after_spec.rb
+++ spec/before_after_spec.rb
@@ -1,19 +1,23 @@
 RSpec.describe 'before and after hooks' do
-  before do
-    p 'before hooks'
+  before(:context) do
+    p 'before context'
   end
 
-  after do
-    p 'after hooks'
-    p @array
-    p @array.clear
+  before(:example) do
+    p 'before example'
+  end
+
+  after(:context) do
+    p 'after context'
+  end
+
+  after(:example) do
+    p 'after example'
   end
 
   it 'test for before & after' do
-    p 'inner example'
-    @array = []
-    p "Inner #{@array}"
-    @array << '1'
     expect(1 + 1).to eq(2)
   end
 end
+
+# context 會在整個測試前執行，也會在測試結束後執行
