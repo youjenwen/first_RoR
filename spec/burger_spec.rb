a86dcef220c47ff05d9cff29543e284c0d879d67
harper.wen <owilalala974@gmail.com>
2023-07-04 09:37:09 +0800

    refactor: add cumstomer msg and agjust before to let

--- spec/burger_spec.rb
+++ spec/burger_spec.rb
@@ -1,18 +1,18 @@
 require './burger'
 
 RSpec.describe Burger do
-
-  before do
-    p "Hello, this is before hooks"
-    @burger = Burger.new('Beef', 'Cheddar')
-  end
+  # 如果我們都認為這個東西是所有 example 都需要使用也可以使用 let! method
+  # 像是 before 一樣
+  let!(:burger) { Burger.new('Pork', 'Cheddar') }
 
   it 'has cheese' do
-    # burger = Burger.new('Beef', 'Cheddar')
-    expect(@burger.cheese).to eq('Cheddar')
+    expect(burger.cheese).to eq('Cheddar')
+    burger.cheese = 'Ricotta'
+    expect(burger.cheese).to eq('Ricotta')
   end
   it 'has meat' do
-    # burger = Burger.new('Beef', 'Cheddar')
-    expect(@burger.meat).to eq('Beef')
+    comparison = 'Beef'
+    # 客製錯誤訊息
+    expect(burger.meat).to eq(comparison), "我想要 #{comparison} 而不是 #{burger.meat}"
   end
 end
